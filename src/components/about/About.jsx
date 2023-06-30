bde557fd8a97f9081e0702de31ada80b3b081ae6
KateMcRo <katemcr0@gmail.com>
2023-06-30 15:51:00 -0700

    about view updated

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,29 @@
-import { useState } from "react";
+import "./about.css"
+import selfie from '../../assets/selfie.jpeg';
 
 export default function About() {
     return(
-        <div>
-         I am the About View
+        <div id="container"> {/* full widht full height flex */}
+            <div id="profile-box">
+                <h1>Hello I'm</h1>
+                <img src={selfie} alt="Kate Rogers"></img>
+                <h2>Kate Rogers</h2>
+                <h3>Web Developer</h3>
+            </div> {/* flex flex column */}
+            <div id="group-box"> {/* flex fflex column */}
+                <div id="title-box">
+                    <h4>ABOUT ME</h4>
+                </div>
+                <div id="about-box">
+                    <p>
+                        I'm a recent UC Berkeley bootcamp student. While new to the field, my passion for coding spans over two decades. With a background in branding & marketing, I decided to pursue coding professionally after consulting for another developer 2 years ago.
+                    <br/><br/>
+                        I take great satisfaction in breaking down complex problems into manageable pieces and creating clean, modular code. Well-organized code is just as beautiful to me as a visually appealing website.
+                    <br/><br/>
+                        What sets me apart is my keen eye for design & that I apply my highly organized nature not only to design but also to my coding practices, ensuring efficiency and maintainability.
+                    </p>
+                </div>
+            </div> 
         </div>
         )
 }
