39e02f42b8d1b17147cf17831e2b244130681eb3
KateMcRo <katemcr0@gmail.com>
2023-07-02 13:18:57 -0700

    updated resume page with pdf download

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,18 +1,55 @@
 import "./resume.css"
+import resume from "../../assets/KateRogers_Resume.png"
 
 export default function Resume() {
-    return(
-        <div class="container">
-            <div class="skillsBox">
+    const onButtonClick = async () => {
+        try {
+          const response = await fetch('KateRogers_Resume.pdf');
+          const blob = await response.blob();
+          const fileURL = window.URL.createObjectURL(blob);
+          let alink = document.createElement('a');
+          alink.href = fileURL;
+          alink.download = 'KateRogers_Resume.pdf';
+          alink.click();
+        } catch (error) {
+          console.error(error);
+        }
+      };
 
-            </div>
-            <div class="resumeBox">
-
-            </div>
-            <div class="signBox">
-
-            </div>
-
+    const skill = [
+      'HTML & CSS',
+      'JavaScript',
+      'jQuery',
+      'Bootstrap',
+      'Node.js',
+      'Express.js',
+      'MongoDB',
+      'React',
+      'SQL',
+      'MySQL'
+    ];
+    
+    return (
+      <div className="container">
+        <div className="skillsBox">
+          <p id="skills">SUMMARY OF SKILLS</p>
+          <ul>
+            {skill.map((skill, index) => (
+              <li key={index}>{skill}</li>
+            ))}
+          </ul>
         </div>
-        )
-}
+        <div className="resumeBox">
+            <img src={resume} id="resumeImg" alt="Kate Rogers Resume" onClick={onButtonClick}></img>
+            <div class="downloadBox" onClick={onButtonClick}>
+                <i class="fa-solid fa-file-arrow-down download"></i> RESUMÉ
+            </div>
+        </div>
+        <div className="signBox">
+            <p id="msg">I look forward to working with you in the future</p>
+            <p id="kr">- Kate Rogers</p>
+        </div>
+      </div>
+    );
+  }
+  
