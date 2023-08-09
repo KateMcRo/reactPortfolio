26f3b184b84fdf0540aa85aa4fc427f24f64c5b0
KateMcRo <katemcr0@gmail.com>
2023-08-09 12:51:20 -0700

    updated responsive styling for portfilio page

--- src/components/projectcard/ProjectCard.jsx
+++ src/components/projectcard/ProjectCard.jsx
@@ -1,17 +1,31 @@
 import React from "react";
-import "./projectcard.css"
+import "./projectcard.css";
 
-export default function ProjectCard ({ title, deployedLink, imgSrc, repo, alt}) {
-    return <div id="cardContainer">
-        <a href={deployedLink}>
-            <img src={imgSrc} alt={alt} className="project roundLR"></img>
+export default function ProjectCard({
+  title,
+  deployedLink,
+  imgSrc,
+  repo,
+  alt,
+}) {
+  return (
+    <div id="cardContainer">
+      <a href={deployedLink}>
+        <img src={imgSrc} alt={alt} className="project roundLR"></img>
+      </a>
+      <br />
+      <div className="flex">
+        <a href={repo}>
+          <i className="fa-brands fa-github"></i>
         </a>
-        <br/>
-        <div className="flex space">
-            <a href={deployedLink} id="projectTitle">{title}</a>
-            <a href={repo}>
-                <i className="fa-brands fa-github"></i>
-            </a>
-        </div>
+        <a
+          href={deployedLink}
+          id="projectTitle"
+          style={{ paddingLeft: "1rem" }}
+        >
+          {title}
+        </a>
+      </div>
     </div>
+  );
 }
