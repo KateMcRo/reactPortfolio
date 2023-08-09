c19982fa20fc351c8aab0c682c4fe15dbad674da
KateMcRo <katemcr0@gmail.com>
2023-08-08 19:29:02 -0700

    updated smart phone styling for about

---
+++ README.md
@@ -0,0 +1,54 @@
+# Kate Rogers | React Portfolio
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Description
+
+Website to share projects I've worked on, my qualifications, and connect with people who are interested in working with me.
+
+- I was motivated to make this project in preparation for entering the workforce.
+- This project allowed me to practice my react skills as well as keeping my components organized.
+- One of the challenges I faced with this project was adding functionality to the contact form that would send messages directly to my email.
+- The implimentation was fairly simple and it was nice to be able to utilize my marketing skills in this project.
+
+## Usage
+
+Navigation is simple and intuitive. Cursor and hover effects were added to help with link visibility. The current section is bolded in the nav bar. Social links are available in the footer.
+
+![About Section](/src/assets/kr-about.png)
+
+Images and titles link to the live project and the github icons link to the corresponding repos. Hover effects were added to help with visibility.
+
+![Portfolio Section](/src/assets/kr-portfolio.png)
+
+Once visitors submit the form, they will see a loading screen and when it completes, I will receive an email with their message. In turn they will receive an email letting them know their message sent and asking for 48 hours to respond.
+
+![Contact Section](/src/assets/kr-contact.png)
+
+Visiters can download a PDF of my resume by clicking on the picture, or the icon and resume text below.
+
+![Resume Section](/src/assets/kr-resume.png)
+
+## License
+
+MIT License
+
+Copyright (c) 2023 Kate Rogers
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,34 +1,79 @@
-import "./about.css"
-import selfie from '../../assets/selfie.jpeg';
+import "./about.css";
+import selfie from "../../assets/selfie.jpeg";
 
 export default function About() {
-    return(
-        <div id="main" className="flex">
-            <div id="profile-box" className="flex column">
-                <h1 className="header">Hello I'm</h1>
-                <div className="image-container">
-                    <img id="aboutImg" src={selfie} alt="Kate Rogers"></img>
-                    <div className="circle-overlay float"></div>
-                </div>    
-                <h2 className="signature">Kate Rogers</h2>
-                <div className="jobTitle">
-                    <h3 className="basic">Web Developer</h3>
-                </div>
-            </div>
-            <div id="group-box" className="flex column">
-                <div id="title-box" className="roundLR flex container basic">
-                    <h4>ABOUT ME</h4>
-                </div>
-                <div id="about-box" className="flex container roundRL">
-                    <p className="body">
-                        I'm a recent UC Berkeley bootcamp student. While new to the field, my passion for coding spans over two decades. With a background in branding & marketing, I decided to pursue coding professionally after consulting for another developer 2 years ago.
-                    <br/><br/>
-                        I take great satisfaction in breaking down complex problems into manageable pieces and creating clean, modular code. Well-organized code is just as beautiful to me as a visually appealing website.
-                    <br/><br/>
-                        What sets me apart is my keen eye for design & that I apply my highly organized nature not only to design but also to my coding practices, ensuring efficiency and maintainability.
-                    </p>
-                </div>
-            </div> 
+  return (
+    <>
+      {/* web view */}
+      <div id="main" className="flex">
+        <div id="profile-box" className="flex column">
+          <h1 className="header">Hello I'm</h1>
+          <div className="image-container">
+            <img id="aboutImg" src={selfie} alt="Kate Rogers"></img>
+            <div className="circle-overlay float"></div>
+          </div>
+          <h2 className="signature">Kate Rogers</h2>
+          <div className="jobTitle">
+            <h3 className="basic">Web Developer</h3>
+          </div>
         </div>
-        )
+        <div id="group-box" className="flex column">
+          <div id="title-box" className="roundLR flex container basic">
+            <h4>ABOUT ME</h4>
+          </div>
+          <div id="about-box" className="flex container roundRL">
+            <p className="body">
+              I'm a recent UC Berkeley bootcamp student. While new to the field,
+              my passion for coding spans over two decades. With a background in
+              branding & marketing, I decided to pursue coding professionally
+              after consulting for another developer 2 years ago.
+              <br />
+              <br />
+              I take great satisfaction in breaking down complex problems into
+              manageable pieces and creating clean, modular code. Well-organized
+              code is just as beautiful to me as a visually appealing website.
+              <br />
+              <br />
+              What sets me apart is my keen eye for design & that I apply my
+              highly organized nature not only to design but also to my coding
+              practices, ensuring efficiency and maintainability.
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* Mobile View */}
+      <div id="mobile">
+        {/* Coral Box */}
+        <div id="m-profile-box" className="flex ">
+          <img id="aboutImg" src={selfie} alt="Kate Rogers"></img>
+          <div className="text-container">
+            <h2 className="signature">Kate Rogers</h2>
+            <h3 className="basic">Web Developer</h3>
+          </div>
+        </div>
+        <div id="m-title-box" className="basic">
+          <h4>ABOUT ME</h4>
+        </div>
+        {/* Magenta Box */}
+        <div id="m-about-box" className="flex container roundRL">
+          <p className="body">
+            I'm a recent UC Berkeley bootcamp student. While new to the field,
+            my passion for coding spans over two decades. With a background in
+            branding & marketing, I decided to pursue coding professionally
+            after consulting for another developer 2 years ago.
+            <br />
+            <br />
+            I take great satisfaction in breaking down complex problems into
+            manageable pieces and creating clean, modular code. Well-organized
+            code is just as beautiful to me as a visually appealing website.
+            <br />
+            <br />
+            What sets me apart is my keen eye for design & that I apply my
+            highly organized nature not only to design but also to my coding
+            practices, ensuring efficiency and maintainability.
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
