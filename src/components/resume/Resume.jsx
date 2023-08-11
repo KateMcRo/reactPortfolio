8c5a6e9dbdea31fdd48e01c361e6b4ad1f77650d
KateMcRo <katemcr0@gmail.com>
2023-08-11 10:26:30 -0700

    updated resume

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,55 +1,61 @@
-import "./resume.css"
-import resume from "../../assets/KateRogers_Resume.png"
+import "./resume.css";
+import resume from "../../assets/resumeSS.png";
 
 export default function Resume() {
-    const onButtonClick = async () => {
-        try {
-          const response = await fetch('KateRogers_Resume.pdf');
-          const blob = await response.blob();
-          const fileURL = window.URL.createObjectURL(blob);
-          let alink = document.createElement('a');
-          alink.href = fileURL;
-          alink.download = 'KateRogers_Resume.pdf';
-          alink.click();
-        } catch (error) {
-          console.error(error);
-        }
-      };
+  const onButtonClick = async () => {
+    try {
+      let alink = document.createElement("a");
+      alink.href = "/KateRogers_Resume.pdf";
+      alink.target = "_blank";
+      alink.download = "KateRogers_Resume.pdf";
+      alink.click();
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    const skill = [
-      'HTML & CSS',
-      'JavaScript',
-      'jQuery',
-      'Bootstrap',
-      'Node.js',
-      'Express.js',
-      'MongoDB',
-      'React',
-      'SQL',
-      'MySQL'
-    ];
-    
-    return (
-      <div className="containerBox flex space">
-        <div className="skillsBox roundLR">
-          <p id="skills" className="basic">SUMMARY OF SKILLS</p>
-          <ul className="basic">
-            {skill.map((skill, index) => (
-              <li key={index}>{skill}</li>
-            ))}
-          </ul>
-        </div>
-        <div className="resumeBox flex column center roundRL">
-            <img src={resume} id="resumeImg" alt="Kate Rogers Resume" onClick={onButtonClick}></img>
-            <div className="downloadBox" onClick={onButtonClick}>
-                <i className="fa-solid fa-file-arrow-down download"></i> RESUMÉ
-            </div>
-        </div>
-        <div className="signBox flex column roundLR">
-            <p id="msg">I look forward to working with you in the future</p>
-            <p id="kr" className="signature">- Kate Rogers</p>
+  const skill = [
+    "HTML & CSS",
+    "JavaScript",
+    "jQuery",
+    "Bootstrap",
+    "Node.js",
+    "Express.js",
+    "MongoDB",
+    "React",
+    "SQL",
+    "MySQL",
+  ];
+
+  return (
+    <div className="containerBox flex space">
+      <div className="skillsBox roundLR">
+        <p id="skills" className="basic">
+          SUMMARY OF SKILLS
+        </p>
+        <ul className="basic">
+          {skill.map((skill, index) => (
+            <li key={index}>{skill}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="resumeBox flex column center roundRL">
+        <img
+          src={resume}
+          id="resumeImg"
+          alt="Kate Rogers Resume"
+          onClick={onButtonClick}
+        ></img>
+        <div className="downloadBox" onClick={onButtonClick}>
+          <i className="fa-solid fa-file-arrow-down download"></i> RESUMÉ
         </div>
       </div>
-    );
-  }
-  
+      <div className="signBox flex column roundLR">
+        <p id="msg">I look forward to working with you in the future</p>
+        <p id="kr" className="signature">
+          - Kate Rogers
+        </p>
+      </div>
+    </div>
+  );
+}
